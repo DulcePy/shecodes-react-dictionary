0c0077fac37bafe9003cb944921a950a32842fb9
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2025-06-06 10:49:08 -0300

    added the words phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <span className="text">{props.phonetic.text}</span> <br />
-      <audio controls>
-        <source src={props.phonetic.audio} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-    </div>
-  );
+  if (!props.phonetic || !props.phonetic.text || !props.phonetic.audio) {
+    return null;
+  } else {
+    return (
+      <div className="Phonetic">
+        <span className="text">{props.phonetic.text}</span> <br />
+        <audio controls>
+          <source src={props.phonetic.audio} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    );
+  }
 }
